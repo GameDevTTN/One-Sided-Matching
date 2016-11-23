82f04f1c38f89bedd33bca917ac6bcb64af6321a
UOA\ylo019 <ylo019@aucklanduni.ac.nz>
2016-11-23 16:03:45 +1300

    Added Skeleton code for RankEff

--- Main/Observers/RankEfficiency.java
+++ Main/Observers/RankEfficiency.java
@@ -5,10 +5,44 @@
  */
 package Main.Observers;
 
+import Graph.GraphImpl;
+import Main.Observers.System.MessageType;
+import MatchingAlgorithm.Auxiliary.iProbabilityMatrix;
+
 /**
  *
  * @author ylo019
  */
-public class RankEfficiency {
+public class RankEfficiency extends iDoubleResultsCollator {
+
+    @Override
+    protected void onEachResult(String key, iProbabilityMatrix value) {
+        //defaultTable.put(key, defaultTable.getOrDefault(key, 0.0) + (new GraphImpl(value, pp).hasCycles() ? 0 : 1));
+    }
+    
+    @Override
+    protected Double onEachEntry(String key) {
+        return getMap().getOrDefault(key, 0.0)/getProfileCount() * 100;
+    }
+    
+    @Override
+    protected MessageType broadcastType() {
+        return MessageType.SUMMARY;
+    }
+    
+    @Override
+    protected String getName() {
+        return "\\%RankEff";
+    }
+    
+    @Override
+    protected String getSuffix() {
+        return "";
+    }
+    
+    @Override
+    protected boolean hasPercentageOutput() {
+        return true;
+    }
     
 }
